878c7cea5e01a905f31d7653d12f7e64c9aa6f7a
Valentina Garzon Perez <valentinagp2006@gmail.com>
2026-07-17 13:09:57 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 1. Cambia "tu-usuario" por tu nombre de usuario de GitHub
+  site: 'https://Valengp2006.github.io', 
+  
+  // 2. Cambia "nombre-del-repo" por el nombre exacto de tu repositorio 
+  // (por ejemplo: '/mi-portafolio' o '/portafolio_1')
+  // IMPORTANTE: Debe llevar la barra diagonal "/" al principio.
+  base: '/Portafolio', 
+  
+  // 3. Esta línea es clave para que las subpáginas funcionen en GitHub Pages
+  trailingSlash: 'always' 
+});
